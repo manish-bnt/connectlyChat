1d63cfdd470f384602b7d07a7e74927fb4f444c3
manish-bnt <manishbasnet500@gmail.com>
2026-03-13 15:52:39 +0530

    credentials added

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:5173",
     origin: [process.env.CLIENT_URL, process.env.LOCAL_URL],
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials:true
   }
 });
 
